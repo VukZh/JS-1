87f66331ff1085d3be9c47c4e07cf19329b1e05e
VukZh <36912856+VukZh@users.noreply.github.com>
2018-03-01 21:59:33 +0600

    Add files via upload

---
+++ prototype.js
@@ -0,0 +1,11 @@
+function createChild(parent){
+	var Fun = function(){};
+	Fun.prototype = parent;
+	return new Fun();
+}
+//var obj1 = {a:1,b:2};
+//obj2=createChild(obj1);
+//obj2.a;
+//obj2.b;
+//obj2.c;
+//obj3.__proto__
